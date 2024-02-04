79047a1fd9cb5bafdb7ab4805f78a7c2e2889b91
vavil <vavilon164@yandex.ru>
2024-02-04 19:27:13 +0300

    Initial

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+	employee_id SERIAL PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders(
+	order_id serial PRIMARY KEY,
+	customer_id varchar(5) UNIQUE REFERENCES customers(customer_id),
+	employee_id SERIAL REFERENCES employees(employee_id),
+	order_date DATE NOT NULL,
+	ship_city varchar(100)
+);
