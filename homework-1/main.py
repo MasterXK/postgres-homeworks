80fc98782aea2d361fd56cc3639ed2f21024b4c4
vavil <vavilon164@yandex.ru>
2024-02-04 19:27:13 +0300

    Initial

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,10 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="mydatabase",
+    user="postgres",
+    password="12345"
+)
